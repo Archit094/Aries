2e37fdd73b387a91b8004d08fe1f8d58f1669b24
Archit094 <f20150094@goa.bits-pilani.ac.in>
2018-12-20 03:26:28 +0530

    Update region_processor.py
    Add comments

--- src/region_growth/region_processor.py
+++ src/region_growth/region_processor.py
@@ -1,3 +1,4 @@
+#Create regions for each second in a single app run and find duration of each region by considering intersection with regions formed in next time step
 import os
 # os.system('module load python/3.6-anaconda-5.2')
 import pandas as pd
